7d0cf703b331e4c2cbd032abb58371c2a3043d6c
IamEunjae <84075869+IamEunjae@users.noreply.github.com>
2025-05-28 23:01:29 +0900

    allocate ip address

--- DormProject/settings.py
+++ DormProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-y-#akc&$+wxct=s-mmn@&s49j^fq579#6y+-z!9$!4##k%s@**
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3.34.60.140', 'locahost']
 
 
 # Application definition
@@ -118,6 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
